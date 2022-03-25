1c1a57468fe45c8d37230f42ad19bc8438844c01
bayarzlac <bayarz.lac@gmail.com>
2022-03-25 19:27:06 +0800

    Articles

---
+++ database/migrations/2022_03_25_103843_create_articles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();            
+            $table->string('title');
+            $table->string('authors');
+            $table->text('abstract_mn');
+            $table->text('abstract_en');
+            $table->string('keywords', 1000);
+            $table->text('full_article_link');
+            $table->dateTime('received');
+            $table->dateTime('revised');
+            $table->dateTime('approved');
+            $table->integer('revised_u_id');
+            $table->boolean('public_publish');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
