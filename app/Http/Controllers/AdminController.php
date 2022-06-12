de07b04fb0d8b287e1328cbb8db0ef558f805812
bayarzlac <34569198+bayarzlac@users.noreply.github.com>
2022-06-12 22:46:26 +0800

    Update AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class AdminController extends Controller
 {
-    //
+    public function __construct() {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
+
         return view('admin.index');
     }
 }
