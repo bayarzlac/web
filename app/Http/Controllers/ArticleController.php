81975a154e80556ca7604ffa74d9f03749a14081
bayarzlac <34569198+bayarzlac@users.noreply.github.com>
2022-07-18 17:13:10 +0800

    update
    file upload to send article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,30 +17,26 @@ class ArticleController extends Controller
 
     public function add(Request $request)
     {
-        // if ($request->hasFile('file'))
-        // {
-            
-        // }
-
         $article = new Articles();
-            $articleUser = new ArticleUsers();
+        $articleUser = new ArticleUsers();
 
-            $article->title = $request->title;
-            $article->authors = $request->authors;
-            $article->abstract_mn = $request->abstract_mn;
-            $article->abstract_en = $request->abstract_en;
-            $article->keywords = $request->keywords;
-            $article->full_article_link = '#';
-            $article->received = Carbon::now();
-            $article->revised = Carbon::now();
-            $article->approved = Carbon::now();
-            $article->revised_u_id = null;
-            $article->u_id = 1;
-            $article->public_publish = 0;
-
-            $article->save();
+        $file = $request->file('file');
+        $filename = date('YmdHi').$file->getClientOriginalName();
+        $file->move(public_path('articles'), $filename);
         
+        $article->title = $request->title;
+        $article->authors = $request->authors;
+        $article->abstract_mn = $request->abstract_mn;
+        $article->abstract_en = $request->abstract_en;
+        $article->keywords = $request->keywords;
+        $article->full_article_link = 'articles/' . $filename;
+        $article->received = Carbon::now();
+        // $article->revised = Carbon::now();
+        // $article->approved = Carbon::now();
+        $article->revised_u_id = null;
+        $article->public_publish = 0;
         
+        $article->save();
 
         return redirect()->route('user.article.new');
     }
--- resources/views/user/article/new.blade.php
+++ resources/views/user/article/new.blade.php
@@ -7,41 +7,41 @@
                     <h2>Өгүүлэл илгээх</h2>
                 </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('user.article.add') }}">
+                    <form method="POST" action="{{ route('user.article.add') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label>Өгүүллийн гарчиг</label>
-                                    <input type="text" name="title" class="form-control" placeholder="" />
+                                    <label>Өгүүллийн гарчиг</label> 
+                                    <input type="text" name="title" class="form-control" placeholder="" required />
                                 </div>
                                 <div class="form-group">
                                     <label>Зохиогч (ид)</label>
                                     <input type="text" name="authors" class="form-control"
-                                        placeholder="Жишээ нь: Жонсон, M. C., Бат, А., Маам, Л." />
+                                        placeholder="Жишээ нь: Жонсон, M. C., Бат, А., Маам, Л." required />
                                     <span class="mt-2 d-block">APA форматаар бичнэ үү.</span>
                                 </div>
 
                                 <div class="form-group">
                                     <label>Өгүүллийн хураангуй (Монгол хэлээр)</label>
-                                    <textarea class="form-control" name="abstract_mn" rows="4"></textarea>
+                                    <textarea class="form-control" name="abstract_mn" rows="4" required></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label>Өгүүллийн хураангуй (Англи хэлээр)</label>
-                                    <textarea class="form-control" name="abstract_en" rows="4"></textarea>
+                                    <textarea class="form-control" name="abstract_en" rows="4" required></textarea>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label>Түлхүүр үгс</label>
-                                    <input type="text" name="keywords" class="form-control" />
+                                    <input type="text" name="keywords" class="form-control" required />
                                     <span class="mt-2 d-block">APA форматаар бичнэ үү.</span>
                                 </div>
 
                                 <div class="form-group">
                                     <label>Өгүүллийн эх файл</label>
-                                    <input type="file" name="file" class="form-control-file" accept="application/pdf">
+                                    <input type="file" name="file" class="form-control-file" accept="application/pdf" required>
                                 </div>
 
                                 <div class="form-footer pt-4 pt-5 mt-4 border-top">
